abb4395b513510d7fd75741d645dfe367010b326
Sekator778 <sekator778@gmail.com>
2021-08-03 09:34:46 +0300

    LinkedList

--- src/main/java/ru/job4j/collection/SimpleArray.java
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -1,7 +1,9 @@
 package ru.job4j.collection;
 
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class SimpleArray<T> implements Iterable<T> {
     private Object[] container;
@@ -9,10 +11,18 @@ public class SimpleArray<T> implements Iterable<T> {
     private int size = 0;
     private int modCount = 0;
 
+    /**
+     * конструктор по дефолту создает внутренний размер
+     */
     public SimpleArray() {
         this.container = new Object[DEFAULT_CAPACITY];
     }
 
+    /**
+     * тут размер мы определяем на этапе создания
+     *
+     * @param initialCapacity - размер внутреннего массива
+     */
     public SimpleArray(int initialCapacity) {
         if (initialCapacity > 0) {
             this.container = new Object[initialCapacity];
@@ -21,6 +31,12 @@ public class SimpleArray<T> implements Iterable<T> {
         }
     }
 
+    /**
+     * получение элемента по индексу
+     *
+     * @param index индекс
+     * @return элемент приведен к типу Т
+     */
     public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Illegal index: " + index);
@@ -28,12 +44,24 @@ public class SimpleArray<T> implements Iterable<T> {
         return (T) container[index];
     }
 
+    /**
+     * добавляем элемент
+     * первым делом смотрим или есть место
+     * если надо т о увеличиваем внутренний массив
+     *
+     * @param model то что добавляем в массив
+     */
     public void add(T model) {
         ensureCapacity(size + 1);
         container[size++] = model;
         modCount++;
     }
 
+    /**
+     * насколько увеличиваем
+     *
+     * @param capacity число - размер увеличенного массива
+     */
     private void ensureCapacity(int capacity) {
         if (capacity > container.length) {
             int newCapacity = container.length == 0 ? 10 : container.length * 2;
@@ -41,12 +69,56 @@ public class SimpleArray<T> implements Iterable<T> {
         }
     }
 
+    /**
+     * копируем массив в новый
+     *
+     * @param newCapacity таким размером
+     */
     private void grow(int newCapacity) {
         container = Arrays.copyOf(container, newCapacity);
     }
 
+    /**
+     * итератор
+     *
+     * @return итератор
+     */
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new SimpleArrayIterator();
+    }
+
+    /**
+     * (expectedModCount != modCount). Если условие выполнено,
+     * значит на момент итерирования была изменена коллекция, поэтому вылетает исключение
+     * внутренний класс который переопределяет поведение методов
+     * интерфейса итератор, а именно:
+     * есть ли следующий ответ да если текущий индекс меньше размера массива
+     * следующий элемент получаем если проходи два условия
+     * 1. что не изменялся массив во время ходьбы по нем
+     * 2. что есть следующий
+     */
+    private class SimpleArrayIterator implements Iterator<T> {
+        private int index;
+        private final int expectedModCount;
+
+        public SimpleArrayIterator() {
+            this.expectedModCount = modCount;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        @Override
+        public T next() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            } else if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return (T) container[index++];
+        }
     }
 }
---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,108 @@
+package ru.job4j.collection.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private Node<E> head;
+    private int size;
+    private int modCount;
+
+
+    private static class Node<E> {
+        private final E currentElement;
+        private Node<E> nextElement;
+        private Node<E> prevElement;
+
+        public Node(E currentElement, Node<E> nextElement, Node<E> prevElement) {
+            this.currentElement = currentElement;
+            this.nextElement = nextElement;
+            this.prevElement = prevElement;
+        }
+
+        public Node<E> getNext() {
+            return nextElement;
+        }
+
+        public void setNext(Node<E> eNode) {
+            this.nextElement = eNode;
+        }
+    }
+
+    /**
+     * если голова списка пустая то добавляем єлемент в голову
+     * если нет то в темп ставим некст элемент от головы пока не будет пустой некст
+     * тогда в некст и цепляем нашего
+     */
+    @Override
+    public void add(E value) {
+        if (head == null) {
+            head = new Node<>(value, null, null);
+        } else {
+            Node<E> temp = head;
+            while (temp.getNext() != null) {
+                temp = head.getNext();
+            }
+            temp.setNext(new Node<E>(value, null, temp));
+        }
+        size++;
+        modCount++;
+    }
+
+    /**
+     * двигаемся по нексту от головы столько раз == индекс
+     */
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        Node<E> temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.nextElement;
+        }
+        return temp.currentElement;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new SimpledLinkedListIterator();
+    }
+
+    /**
+     * стартуем из головы Node<E> node = head
+     * также на момент создания фиксируем this.currenModCount = modCount
+     */
+    private class SimpledLinkedListIterator implements Iterator<E> {
+        private Node<E> node = head;
+        private int currenModCount;
+
+        public SimpledLinkedListIterator() {
+            this.currenModCount = modCount;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return node != null;
+        }
+
+        /**
+         * отдаем значение текущего нода и ставим в нода следующего
+         * E value = node.currentElement;
+         * node = node.nextElement;
+         */
+        @Override
+        public E next() {
+            if (currenModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            E value = node.currentElement;
+            node = node.nextElement;
+            return value;
+        }
+    }
+}
