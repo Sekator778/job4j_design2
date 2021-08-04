5702d88eecdd6e6e0febeedf3e53b66b6bf2c09f
Sekator778 <sekator778@gmail.com>
2021-08-04 18:07:14 +0300

    io

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+
+//        List<String> petNames = humans.stream()
+//                .map(human -> human.getPets()) //преобразовываем Stream<Human> в Stream<List<Pet>>
+//                .flatMap(pets -> pets.stream())//"разворачиваем" Stream<List<Pet>> в Stream<Pet>
+//                .collect(Collectors.toList());
+
+
+        List<String> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Temp\\" + file))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                var chars = line.split(" ");
+                if (chars[chars.length - 2].contains("404")) {
+                    result.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        log.forEach(System.out::println);
+    }
+}
