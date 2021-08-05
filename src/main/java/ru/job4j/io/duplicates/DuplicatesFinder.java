9c056ef33074c28ffe3326b3cda2e473823a3562
Sekator778 <sekator778@gmail.com>
2021-08-05 17:37:16 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -12,6 +12,9 @@ import java.nio.file.Path;
  */
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar target/duplicatesFinder.jar ROOT_FOLDER.");
+        }
         Files.walkFileTree(Path.of(args[0]), new DuplicatesVisitor());
     }
 }
