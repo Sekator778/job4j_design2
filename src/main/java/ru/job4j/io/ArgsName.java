320b79abbff117fe0157a13df07ad5cb12cf70ea
Sekator778 <sekator778@gmail.com>
2021-08-05 17:53:15 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * программу, которая принимает массив параметров и разбивает их на пары: ключ, значение.
+ */
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("NO args !!!");
+        }
+        for (String s : args
+        ) {
+            var strings = s.split("=");
+            if (strings.length < 2) {
+                throw new IllegalArgumentException("args invalid: Example key=value");
+            }
+            values.put(strings[0].substring(1), strings[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
