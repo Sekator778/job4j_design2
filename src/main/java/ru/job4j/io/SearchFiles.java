fb2808c97046a7df96e14cdf0fc57987b51c7c4a
Sekator778 <sekator778@gmail.com>
2021-08-05 14:27:28 +0300

    search file in dir

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    private Predicate<Path> predicate;
+    private List<Path> pathList = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.predicate = condition;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (predicate.test(file)) {
+            pathList.add(file.getFileName());
+        }
+        return CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return pathList;
+    }
+}
