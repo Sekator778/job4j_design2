b4d29524c280285d8a23dcea2050ca317c9ed67f
Sekator778 <sekator778@gmail.com>
2021-08-11 17:27:51 +0300

    Scanner2

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -12,8 +12,12 @@ public class CSVReader {
     private static String path;
     private static String delimiter;
     private static String out;
-    private static String[] filters;
+    private static List<String> filters;
+    private static final List<String> STRINGS = new ArrayList<>();
 
+    /**
+     * с получением аргументов не совсем пока хорошо
+     */
     public static void main(String[] args) throws FileNotFoundException {
         argsName = ArgsName.of(args);
         argSet = argsName.getKeys();
@@ -29,44 +33,70 @@ public class CSVReader {
     /**
      * считываем файл
      */
-    private static List<String> scannerCSV() throws FileNotFoundException {
-        List<String> output = new ArrayList<>();
-        try (var scanner = new Scanner(new File(path))) {
-            scanner.useDelimiter(delimiter);
-            while (scanner.hasNext()) {
-                output.add(scanner.next() + " | ");
+    private static List<String> scannerCSV2() {
+        List<Integer> index = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(path))) {
+            Scanner dataScanner;
+            int i = 0;
+            int line = 0;
+            while (scanner.hasNextLine()) {
+                dataScanner = new Scanner(scanner.nextLine());
+                dataScanner.useDelimiter(delimiter);
+                if (line == 0) {
+                    String firstLine = dataScanner.nextLine();
+                    index = getIndexFilter(firstLine);
+                    line++;
+                    continue;
+                } else {
+                    line++;
+                }
+                while (dataScanner.hasNext()) {
+                    String data = dataScanner.next();
+                    if (index.contains(i)) {
+                        STRINGS.add(data + "|");
+                    }
+                    i++;
+                }
+                STRINGS.add("\n");
+                i = 0;
             }
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return output;
+        return STRINGS;
     }
 
     /**
-     * оставить только те что по фильтру
+     * достаем индексы столбцов которые нам надо вывести
+     * предикат фильтра
+     * заполнение результата заглавием колонок
      */
-    private  static List<String> filterCsv() throws FileNotFoundException {
-        List<String> result = new ArrayList<>();
-        result = scannerCSV();
-        System.out.println("==================");
-        System.out.println(result.get(0));
-        return null;
+    private static List<Integer> getIndexFilter(String firstLine) {
+        List<String> first;
+        List<Integer> index = new ArrayList<>();
+        first = Arrays.asList(firstLine.split(", "));
+        for (int i = 0; i < first.size(); i++) {
+            if (filters.contains(first.get(i))) {
+                index.add(i);
+                STRINGS.add(first.get(i) + "|");
+            }
+        }
+        STRINGS.add("\n");
+        return index;
     }
-
-
 
     /**
      * вывод в консоль
      */
-    private static void outToConsole() throws FileNotFoundException {
-        scannerCSV().forEach(System.out::print);
+    private static void outToConsole() {
+        scannerCSV2().forEach(System.out::print);
     }
 
     /**
      * вывод в файл
      */
-    private static void outToFile() throws FileNotFoundException {
-        List<String> outData = scannerCSV();
+    private static void outToFile() {
+        List<String> outData = scannerCSV2();
         Random random = new Random(10);
         try (BufferedWriter reader = new BufferedWriter(new FileWriter(out + random.nextInt() + ".txt"))) {
             for (String line : outData) {
@@ -96,6 +126,7 @@ public class CSVReader {
         path = argsName.get("path");
         delimiter = argsName.get("delimiter");
         out = argsName.get("out");
-        filters = argsName.get("filter").split(",");
+        filters = Arrays.asList(argsName.get("filter").split(","));
+
     }
 }
