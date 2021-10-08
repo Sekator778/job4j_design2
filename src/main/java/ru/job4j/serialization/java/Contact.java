f9d7286ec14df13728eb0f984ab56bcf44f91851
Sekator778 <sekator778@gmail.com>
2021-10-08 18:01:08 +0300

    check

---
+++ src/main/java/ru/job4j/serialization/java/Contact.java
@@ -0,0 +1,54 @@
+package ru.job4j.serialization.java;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public class Contact implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 511475673970270198L;
+    private final int zipCode;
+    private final String phone;
+    transient private final int amount;
+    private final Contain contain = new Contain();
+
+    public Contact(int zipCode, String phone, int amount) {
+        this.zipCode = zipCode;
+        this.phone = phone;
+        this.amount = amount;
+    }
+
+    public int getContain() {
+        return contain.containVersion;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" + "zipCode=" + zipCode + ", phone='" + phone + '\'' + ", amount='" + amount + '}';
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(123456, "+7 (111) 111-11-11", 777);
+
+        /* Запись объекта во временный файл, который удалится системой */
+        String nameFile = "C:\\Temp\\out.pol";
+        File tempFile = Files.createTempFile(null, null).toFile();
+        try (FileOutputStream fos = new FileOutputStream(nameFile);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            System.out.println("Before serialization");
+            System.out.println(contact);
+            oos.writeObject(contact);
+        }
+        /* Чтение объекта из файла */
+        try (FileInputStream fis = new FileInputStream(nameFile);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            final Contact contactFromFile = (Contact) ois.readObject();
+            System.out.println("After serialization");
+            System.out.println(contactFromFile);
+            System.out.println(contactFromFile.getContain());
+        }
+    }
+
+    static class Contain implements Serializable {
+        int containVersion = 11;
+    }
+}
