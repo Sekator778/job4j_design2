7f9445582ed14d6bace39949a96da902c30a20b9
Sekator778 <sekator778@gmail.com>
2021-10-10 12:06:22 +0300

    2

--- src/main/java/ru/job4j/filefinder/Finder.java
+++ src/main/java/ru/job4j/filefinder/Finder.java
@@ -59,16 +59,16 @@ public class Finder {
         return switch (type) {
             case "name" -> path -> name.equals(path.getFileName().toString());
             case "mask" -> path -> {
-                StringBuilder builder = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
                 for (int i = 0; i < name.length(); i++) {
                     switch (name.charAt(i)) {
-                        case '*' -> builder.append(".*");
-                        case '?' -> builder.append(".{1}");
-                        case '.' -> builder.append("\\.");
-                        default -> builder.append(name.charAt(i));
+                        case '*' -> sb.append(".*");
+                        case '?' -> sb.append(".{1}");
+                        case '.' -> sb.append("\\.");
+                        default -> sb.append(name.charAt(i));
                     }
                 }
-                Pattern pattern = Pattern.compile(builder.toString());
+                Pattern pattern = Pattern.compile(sb.toString());
                 Matcher matcher = pattern.matcher(path.toFile().getName());
                 return matcher.matches();
             };
