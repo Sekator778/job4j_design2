7ab7192a1d7062abba4e2b82bf2f827c3a37eb6f
Sekator778 <sekator778@gmail.com>
2021-10-25 14:48:12 +0300

    0.2. PrepareStatement [#379307]

---
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -0,0 +1,90 @@
+package ru.job4j.spammer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * клиент, который хочет создать базу данных для спамеров.
+ * <p>
+ * На рынке ему продали диск, в котором находятся txt файлы.
+ * <p>
+ * Формат данных dump.txt:
+ * <p>
+ * Petr Arsentev;parsentev@yandex.ru;
+ * Ivan Ivanov;iivanov@gmail.com;
+ * <p>
+ * Клиент просит перевести эти файлы в базу данных PostgreSQL.
+ */
+public class ImportDB {
+
+    private final Properties cfg;
+    private final String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines().forEach(line -> {
+                        String[] strings = line.split(";");
+                        users.add(new User(strings[0], strings[1]));
+                    }
+            );
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into userspamer(name, email) values (?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        @Override
+        public String toString() {
+            return "User name: " + name + " -- " + "email: " + email + ";";
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("src/main/java/ru/job4j/spammer/app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "src/main/java/ru/job4j/spammer/dump.txt");
+        db.save(db.load());
+        db.load().forEach(System.out::println);
+    }
+}
