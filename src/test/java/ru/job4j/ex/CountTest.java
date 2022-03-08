ddc9f46d6cc163b13b4c7d79f8dee30e57764433
Sekator778 <sekator778@gmail.com>
2022-03-08 21:34:55 +0200

    ValidateInput

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CountTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenStartGreaterThenFinish() {
+        Count.add(10, 2);
+    }
+
+    @Test
+    public void when0to2then3() {
+        int rsl = Count.add(0, 3);
+        assertThat(rsl, is(3));
+    }
+}
