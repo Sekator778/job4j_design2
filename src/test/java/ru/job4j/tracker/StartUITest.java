2ebab69cae2057f85a65f39a0b8e9eb202beebbe
Sekator778 <sekator778@gmail.com>
2022-03-08 13:55:28 +0200

    jar

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,48 @@
+package ru.job4j.tracker;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.*;
+
+public class StartUITest {
+    @Test
+    public void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()) /* id сохраненной заявки в объект tracker. */
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item delItem = tracker.findById(item.getId());
+        assertThat(delItem, Matchers.is(nullValue()));
+    }
+}
