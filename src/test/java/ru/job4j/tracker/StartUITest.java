1acc030e0a6faa5fc35a1f627fcb89789a64e777
Sekator778 <sekator778@gmail.com>
2022-03-08 21:39:45 +0200

    Validate wrong Input

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -100,4 +100,25 @@ public class StartUITest {
                         + "1. === Choose for EXIT ===" + ln
         ));
     }
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"3", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                        "Menu:" + ln
+                                + "0. === Choose for EXIT ===" + ln
+                                + "Wrong input, you can select: 0 .. 0" + ln
+                                + "Menu:" + ln
+                                + "0. === Choose for EXIT ===" + ln
+                )
+        );
+    }
 }
