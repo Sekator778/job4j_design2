d4ffe5ea36c30876301342d74aba89739b2d26ea
Sekator778@ <sekator778@gmail.com>
2022-03-13 12:59:53 +0200

    anonymous class

---
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -0,0 +1,31 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 3", 13),
+                new Attachment("image 2", 34)
+        );
+        Comparator<Attachment> comparatorSize = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return Integer.compare(left.getSize(), right.getSize());
+            }
+        };
+        attachments.sort(comparatorSize);
+        System.out.println(attachments);
+        Comparator<Attachment> comparatorName = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getName().compareTo(right.getName());
+            }
+        };
+        attachments.sort(comparatorName);
+        System.out.println(attachments);
+    }
+}
