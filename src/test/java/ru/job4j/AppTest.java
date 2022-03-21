c0f762ca9dbf43c4b8341781088871080e4312e9
Sekator778@ <sekator778@gmail.com>
2022-03-21 08:37:19 +0200

    ci bot

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -35,15 +35,10 @@ public class AppTest
     public static String binary(int num) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 31; i++) {
-            sb.append(num % 2 == 0 ? 0 : 1);
+            sb.append((num % 2 == 0) ? 0 : 1);
             sb.append((i + 1) % 8 == 0 ? " " : "");
             num /= 2;
         }
         return sb.reverse().toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(31 % 15);
-        System.out.println(binary(6));
     }
 }
